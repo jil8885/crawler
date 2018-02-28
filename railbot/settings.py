010945973a30378279bc85469cc229d7212c2671
jil8885 <jil8885@gmail.com>
2018-02-28 11:47:21 +0900

    Debug Code

--- railbot/settings.py
+++ railbot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#$wb!)3xk(8=3cnbdoaac%(h&d$k8#jih%$a$_@j)8l!@2hi5c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
