08bcb05abd3f623bb0c3abac427224401efbdcd1
jeongin <jil8885@hanyang.ac.kr>
2018-07-23 17:14:33 +0900

    extract more info

--- bus_stop.py
+++ bus_stop.py
@@ -8,5 +8,11 @@ for x in range(100000):
     req = requests.get(link + str(x).zfill(5))
     soup = BeautifulSoup(req.content, "lxml-xml")
     result = soup.find_all('busStationList')
-    print(len(result))
+    for x in result:
+        content = BeautifulSoup(str(x), "lxml-xml")
+        region = content.find('regionName').string
+        name = content.find('stationName').string
+        stnid = content.find('stationId').string
+        print(region, name, stnid)
+        time.sleep(1)
     time.sleep(1)
